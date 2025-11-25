278b296148b51fe281b28da1918913fe4efe2bc0
OkazakiTakuma <k024g0021@g.neec.ac.jp>
2025-11-25 10:48:25 +0900

    敵を倒したら変わるように修正

--- StageScene.cpp
+++ StageScene.cpp
@@ -1,19 +1,127 @@
 #include "StageScene.h"
 #include <Novice.h>
 
-void StageScene::Initialize() {}
+void StageScene::Initialize() {
+	enmyResporn = 120;
+	isAlive = 1;
+	radiusEnemy = 15;
+	enemyPosX = 640;
+	enemyPosY = 20;
+	enemySpeedX = 3;
+
+	preyerPosX = 640;
+	preyerPosY = 360;
+	preyerSpeedX = 5;
+	preyerSpeedY = 5;
+	heightWeight = 20;
+
+	bulletPosX = 0;
+	bulletPosY = 0;
+	isBullet = 0;
+
+}
 
 void StageScene::Update(char* keys, char* preKeys) {
 
-	// スペースキーで次のシーン（クリア）へ
-	if (preKeys[DIK_SPACE] == 0 && keys[DIK_SPACE] != 0) {
+	if (keys[DIK_W]) {
 
+		preyerPosY = preyerPosY - preyerSpeedY;
+	}
+	if (keys[DIK_A]) {
+
+		preyerPosX = preyerPosX - preyerSpeedX;
+	}
+	if (keys[DIK_S]) {
+
+		preyerPosY = preyerPosY + preyerSpeedY;
+	}
+	if (keys[DIK_D]) {
+
+		preyerPosX = preyerPosX + preyerSpeedX;
+	}
+	if (keys[DIK_SPACE] && !preKeys[DIK_SPACE]) {
+		isBullet = 1;
+	}
+
+	/// ↓更新処理ここから
+	// 画面外不可
+	if (preyerPosX > 1280 - heightWeight) {
+		preyerPosX = 1280 - heightWeight;
+	}
+	if (preyerPosY > 720 - heightWeight) {
+		preyerPosY = 720 - heightWeight;
+	}
+	if (preyerPosX < 0 + heightWeight) {
+		preyerPosX = 0 + heightWeight;
+	}
+	if (preyerPosY < 0 + heightWeight) {
+		preyerPosY = 0 + heightWeight;
+	}
+	// 判定
+	float diffrence_e2b_X = (bulletPosX - enemyPosX);
+	float diffrence_e2b_Y = (bulletPosY - enemyPosY);
+	float diffrence_e2b = diffrence_e2b_X * diffrence_e2b_X + diffrence_e2b_Y * diffrence_e2b_Y;
+	float e2b_Radius = radiusEnemy + 10;
+	if (e2b_Radius * e2b_Radius >= diffrence_e2b) {
+		isAlive = 0;
 		sceneNo = CLEAR;
+	}
+
+	// 敵フラグ
+	if (isAlive == 0) {
+		enmyResporn = enmyResporn - 1;
+	}
+	if (enmyResporn == 0) {
+		isAlive = 1;
+	}
+	// 玉座標あわせ
+	if (isBullet == 0) {
+		bulletPosY = preyerPosY;
+		bulletPosX = preyerPosX + heightWeight / 2.0f;
+	}
+
+	///
+	/// ↑更新処理ここまで
+	///
+
+	///
+	/// ↓描画処理ここから
+	///
+
+	if (isAlive == 1) {
+		enmyResporn = 120;
+		enemyPosX = enemyPosX + enemySpeedX;
+
+		if (enemyPosX > 1280 - radiusEnemy) {
+			enemySpeedX = -enemySpeedX;
+		}
+		if (enemyPosX < 0 + radiusEnemy) {
+			enemySpeedX = -enemySpeedX;
+		}
+	}
+
+	if (isBullet == 1) {
+
+		bulletPosY = bulletPosY - 20;
+	}
+
+	if (bulletPosY <= -10) {
+		isBullet = 0;
 	}
 }
 
 void StageScene::Draw() {
-
+	if (isAlive == 1) {
+		Novice::DrawEllipse(int(enemyPosX), int(enemyPosY), int(radiusEnemy), int(radiusEnemy), 0.0f, 0xffffffff, kFillModeSolid);
+	}
+	if (isBullet == 1) {
+		float bulletPosXLeft = bulletPosX - 8;
+		float bulletPosXRight = bulletPosX + 8;
+		float bulletPosYBottom = bulletPosY + 10;
+		float bulletPosYUp = bulletPosY - 8;
+		Novice::DrawTriangle(int(bulletPosX), int(bulletPosYUp), int(bulletPosXLeft), int(bulletPosYBottom), int(bulletPosXRight), int(bulletPosYBottom), 0Xff0000ff, kFillModeSolid);
+	}
 	Novice::ScreenPrintf(0, 0, "Curernt Scene : Stage");
 	Novice::ScreenPrintf(0, 30, "Push Space : Next Scene");
+	Novice::DrawBox(int(preyerPosX), int(preyerPosY), int(heightWeight), int(heightWeight), 0.0f, 0xffffffff, kFillModeSolid);
 }
--- StageScene.h
+++ StageScene.h
@@ -1,5 +1,6 @@
-#pragma once
+//#pragma once
 #include "IScene.h"
+#include <Novice.h>
 
 class StageScene : public IScene {
 
@@ -7,4 +8,21 @@ public:
 	void Initialize() override;
 	void Update(char* keys, char* preKeys) override;
 	void Draw() override;
+	int enmyResporn = 120;
+	int isAlive = 1;
+	float radiusEnemy = 15;
+	float enemyPosX = 640;
+	float enemyPosY = 20;
+	float enemySpeedX = 3;
+
+	// 自機
+	float preyerPosX = 640;
+	float preyerPosY = 360;
+	float preyerSpeedX = 5;
+	float preyerSpeedY = 5;
+	float heightWeight = 20;
+	// 玉
+	float bulletPosX = 0;
+	float bulletPosY = 0;
+	float isBullet = 0;
 };
